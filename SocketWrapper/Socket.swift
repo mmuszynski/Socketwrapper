4f3d8db6730e08ba5fa977c44e9ef7dce0de07f5
Mike Muszynski <mmuszynski@gmail.com>
2017-07-31 09:54:33 -0400

    added socket class

---
+++ SocketWrapper/Socket.swift
@@ -0,0 +1,9 @@
+//
+//  Socket.swift
+//  SocketWrapper
+//
+//  Created by Mike Muszynski on 7/31/17.
+//  Copyright © 2017 Mike Muszynski. All rights reserved.
+//
+
+import Foundation
---
+++ SocketWrapper/SocketWrapper.h
@@ -0,0 +1,19 @@
+//
+//  SocketWrapper.h
+//  SocketWrapper
+//
+//  Created by Mike Muszynski on 7/31/17.
+//  Copyright © 2017 Mike Muszynski. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SocketWrapper.
+FOUNDATION_EXPORT double SocketWrapperVersionNumber;
+
+//! Project version string for SocketWrapper.
+FOUNDATION_EXPORT const unsigned char SocketWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SocketWrapper/PublicHeader.h>
+
+
