f4632ab4774afbb04f7db62b83e02c9a2476b2b7
Mike Muszynski <mmuszynski@gmail.com>
2020-11-15 18:57:29 -0500

    renamed for spm

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "socketwrapper",
+    name: "SocketWrapper",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "socketwrapper",
-            targets: ["socketwrapper"]),
+            name: "SocketWrapper",
+            targets: ["SocketWrapper"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "socketwrapper",
+            name: "SocketWrapper",
             dependencies: []),
         .testTarget(
-            name: "socketwrapperTests",
-            dependencies: ["socketwrapper"]),
+            name: "SocketWrapperTests",
+            dependencies: ["SocketWrapper"]),
     ]
 )
